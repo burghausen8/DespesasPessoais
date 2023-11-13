98511628bf1d8ac2c62d7cd19df720360cdd054c
burghausen8 <burghausen8@gmail.com>
2023-11-13 19:06:09 -0300

    feat: create class transactions

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expenses/Models/transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(ExpensesApp());
@@ -10,15 +11,34 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final _transactions = [
+    Transaction(
+        id: '1',
+        title: 'novo tenis de corrida',
+        value: 310.76,
+        date: DateTime.now()),
+    Transaction(
+        id: '2',
+        title: 'conta de luz cara',
+        value: 120.56,
+        date: DateTime.now())
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Despesas Pessoais'),
-      ),
-      body: Center(
-        child: Text('Versão Inicial'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Despesas Pessoais'),
+        ),
+        body: Column(
+          children: [
+            Card(
+              child: Text('Grafico'),
+            ),
+            Card(
+              child: Text('Lista de Transações'),
+            )
+          ],
+        ));
   }
 }
