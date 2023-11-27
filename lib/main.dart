39393a27490f703a2cf09cf01e3da4f2e854bb18
burghausen8 <burghausen8@gmail.com>
2023-11-27 18:19:26 -0300

    add dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expenses/Models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(ExpensesApp());
 
@@ -66,7 +67,7 @@ class MyHomePage extends StatelessWidget {
                                 fontSize: 16, fontWeight: FontWeight.bold),
                           ),
                           Text(
-                            tr.date.toString(),
+                            DateFormat('d MMM y').format(tr.date),
                             style: TextStyle(color: Colors.grey),
                           )
                         ],
