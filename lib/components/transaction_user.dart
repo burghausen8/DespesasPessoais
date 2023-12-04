d58ed299d3f6f7998a8fd18ab78696774f9675dc
burghausen8 <burghausen8@gmail.com>
2023-12-04 18:13:15 -0300

    feat: modularizando app

---
+++ lib/components/transaction_user.dart
@@ -0,0 +1,31 @@
+import 'package:expenses/models/transaction.dart';
+import 'package:flutter/material.dart';
+import 'transaction_form.dart';
+import 'transaction_list.dart';
+
+class TransactionUser extends StatefulWidget {
+  @override
+  State<TransactionUser> createState() => _TransactionUserState();
+}
+
+class _TransactionUserState extends State<TransactionUser> {
+  final _transactions = [
+    Transaction(
+        id: '1',
+        title: 'novo tenis de corrida',
+        value: 310.76,
+        date: DateTime.now()),
+    Transaction(
+        id: '2',
+        title: 'conta de luz cara',
+        value: 120.56,
+        date: DateTime.now())
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [TransactionList(_transactions), TransactionForm()],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:expenses/Models/transaction.dart';
+import 'package:expenses/components/transaction_user.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 void main() => runApp(ExpensesApp());
 
@@ -12,22 +11,6 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final titleController = TextEditingController();
-  final valueController = TextEditingController();
-
-  final _transactions = [
-    Transaction(
-        id: '1',
-        title: 'novo tenis de corrida',
-        value: 310.76,
-        date: DateTime.now()),
-    Transaction(
-        id: '2',
-        title: 'conta de luz cara',
-        value: 120.56,
-        date: DateTime.now())
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,70 +22,7 @@ class MyHomePage extends StatelessWidget {
             Container(
               child: Card(child: Text('Grafico')),
             ),
-            Column(
-              children: _transactions.map((tr) {
-                return Card(
-                  child: Row(
-                    children: [
-                      Container(
-                        margin:
-                            EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                        child: Text(
-                          'R\$ ${tr.value.toStringAsFixed(2)}',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.purple,
-                          ),
-                        ),
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                          color: Colors.purple,
-                          width: 2,
-                        )),
-                        padding: EdgeInsets.all(10),
-                      ),
-                      Column(
-                        children: [
-                          Text(
-                            tr.title,
-                            style: TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.bold),
-                          ),
-                          Text(
-                            DateFormat('d MMM y').format(tr.date),
-                            style: TextStyle(color: Colors.grey),
-                          )
-                        ],
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                      )
-                    ],
-                  ),
-                );
-              }).toList(),
-            ),
-            Card(
-              elevation: 5,
-              child: Padding(
-                padding: const EdgeInsets.all(10),
-                child: Column(
-                  children: [
-                    TextField(
-                      controller: titleController,
-                      decoration: InputDecoration(labelText: 'Titulo'),
-                    ),
-                    TextField(
-                        controller: valueController,
-                        decoration: InputDecoration(labelText: 'Valor (R\$)')),
-                    TextButton(
-                      onPressed: () {},
-                      child: Text('Nova Transação'),
-                      style: TextButton.styleFrom(primary: Colors.purple),
-                    ),
-                  ],
-                ),
-              ),
-            )
+            TransactionUser()
           ],
         ));
   }
