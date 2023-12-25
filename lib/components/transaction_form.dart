750044eab74a07a6e9742def0ff2ccdd215c6a3e
burghausen8 <burghausen8@gmail.com>
2023-12-25 14:53:56 -0300

    feat: implement callback in transaction_form

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -4,6 +4,10 @@ class TransactionForm extends StatelessWidget {
   final titleController = TextEditingController();
   final valueController = TextEditingController();
 
+  final void Function(String, double) onSubmit;
+
+  TransactionForm(this.onSubmit);
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -20,7 +24,12 @@ class TransactionForm extends StatelessWidget {
                 controller: valueController,
                 decoration: InputDecoration(labelText: 'Valor (R\$)')),
             TextButton(
-              onPressed: () {},
+              onPressed: () {
+                final title = titleController.text;
+                final value = double.tryParse(valueController.text) ?? 0.0;
+
+                onSubmit(title, value);
+              },
               child: Text('Nova Transação'),
               style: TextButton.styleFrom(primary: Colors.purple),
             ),
